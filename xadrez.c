d493294221f87c0088b6ec94c6b1b4403d311052
Blxck7 <guilhermelopessouza2017@gmail.com>
2026-06-25 17:25:17 -0300

    NÍVEL MESTRE CONCLUÍDO!!!

--- xadrez.c
+++ xadrez.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 
+    //Recursividade para mover a Torre
+void moverTorre(int casas){
+    if (casas > 0){
+        printf("Direita\n");
+        moverTorre(casas - 1);
+    }
+}
+    //Recursividade para mover o Bispo
+void moverBispo(int casas){
+    if (casas > 0){
+        for (int vertical = 0; vertical < 1; vertical++){
+            printf("Cima, ");
+            for (int horizontal = 0; horizontal < 1; horizontal++){
+                printf("Direta\n");
+            }
+        }
+        moverBispo(casas - 1);
+    }
+}
+    //Recursividade para mover a Rainha
+void moverRainha(int casas){
+    if (casas > 0){
+        printf("Esquerda\n");
+        moverRainha(casas - 1);
+    }
+}
+
 int main(){
     // Variáveis
     int escolhaJogador;
-    int indicador = 0;
+
 
     // A escolha de qual peça o jogador vai usar
     printf("Escolha a peça:\n");
@@ -16,27 +43,19 @@ int main(){
     // Definição de movimentos de cada peça
     switch (escolhaJogador) {
         case 1:
-            for (int i = 0; i < 5; i++){
-                printf("Direita\n");
-            }
+            moverTorre(5);
         break;
         case 2:
-            while (indicador < 5){
-                printf("Cima-Direita\n");
-                indicador++;
-            }
+            moverBispo(5);
         break;
         case 3:
-            do {
-                printf("Esquerda\n");
-                indicador++;
-            } while (indicador < 8);
+            moverRainha(8);
         break;
         case 4:
-            for (int i = 1; i <= 2; i++){ // Esquema para repetir duas vezes a movimentação para baixo.
-                printf("Baixo\n");
-                for (int j = 1; i == 2; i++){ // Esquema para a movimentação ir para esquerda, só quando o i <= 2.
-                    printf("Esquerda\n");
+            for (int passoLongo = 1; passoLongo <= 2; passoLongo++){ // Esquema para repetir duas vezes a movimentação para baixo.
+                printf("Cima\n");
+                for (int passoCurto = 1; passoLongo == 2; passoLongo++){ // Esquema para a movimentação ir para esquerda, só quando o passoLongo <= 2.
+                    printf("Direita\n");
                 }
             }
         }
